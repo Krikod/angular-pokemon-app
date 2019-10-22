aade260d6df8f22bc368be2162570f4917dfba1b
Krikod <kristen.kod@gmail.com>
2019-10-22 12:35:19 +0200

    create Class & import in Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { Pokemon } from './pokemon'; // pas ext car ts va compiler
 
 @Component({
   selector: 'pokemon-app',
   template: `<h1>Bonjour... {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Angular'; 
+
+  private pokemons: Pokemon[]; 
+  // propr pokemons, typée comme tab.de type Pok
+}
