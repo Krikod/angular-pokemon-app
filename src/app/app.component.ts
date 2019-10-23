b71e82d8d2ff3a4088ffb0ca259c9feb320264ad
Krikod <kristen.kod@gmail.com>
2019-10-23 11:39:15 +0200

    implements interface OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 import { Pokemon } from './pokemon'; // pas ext car ts va compiler
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
   selector: 'pokemon-app',
   template: `<h1>Bonjour... {{name}}</h1>`,
 })
-export class AppComponent  { 
+export class AppComponent implements OnInit { 
   name = 'Angular'; 
 
   private pokemons: Pokemon[]; 
   // propr pokemons, typée comme tab.de type Pok
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+      this.pokemons = POKEMONS;
+  }
 }
